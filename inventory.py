47a46d0ba3472306f6b4e1556e0324d0ed0764b7
A3incSTIGMAT <brilsash@yandex.ru>
2025-01-16 19:07:51 +0800

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,21 +1,3 @@
-# inventory.py
-
-# Классы для оружия и брони
-class Weapon:
-    def __init__(self, name, damage, cost, weapon_type):
-        self.name = name
-        self.damage = damage
-        self.cost = cost
-        self.weapon_type = weapon_type
-
-class Armor:
-    def __init__(self, name, defense, cost, armor_type):
-        self.name = name
-        self.defense = defense
-        self.cost = cost
-        self.armor_type = armor_type
-
-# Класс инвентаря
 class Inventory:
     def __init__(self):
         self.items = []
@@ -29,4 +11,5 @@ class Inventory:
 
     def show_inventory(self):
         return [item.name for item in self.items]
+
 
