85f5e8c35dd549c74e4b5d6c217929b78c2ed724
A3incSTIGMAT <brilsash@yandex.ru>
2025-01-16 15:16:43 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,14 +34,20 @@ dp = Dispatcher(bot)
 async def start_handler(message: types.Message):
     await message.reply("Привет! Готов к PvP-сражениям!")
 
+# Хэндлер для команды /battle
+@dp.message_handler(commands=["battle"])
+async def battle_handler(message: types.Message):
+    await message.reply("PvP-сражение пока в разработке!")
+
 # Хэндлер для команды /help
 @dp.message_handler(commands=["help"])
 async def help_handler(message: types.Message):
     help_text = (
         "/start - Начать игру или взаимодействие с ботом\n"
+        "/battle - Вызвать игрока на PvP\n"
+        "/help - Список всех команд\n"
         "/rules - Правила игры\n"
         "/about - О боте\n"
-        "/battle - Вызвать игрока на PvP\n"
         "/accept - Принять вызов\n"
         "/attack - Атаковать соперника\n"
         "/defend - Защититься от атаки\n"
@@ -67,122 +73,12 @@ async def help_handler(message: types.Message):
 # Хэндлер для команды /rules
 @dp.message_handler(commands=["rules"])
 async def rules_handler(message: types.Message):
-    rules_text = "Правила игры:\n1. Каждый игрок должен вызвать другого на PvP.\n2. Ожидайте атаки и защищайтесь.\n3. Побеждает тот, кто выживет."
-    await message.reply(rules_text)
+    await message.reply("Правила игры: Взаимодействуйте с ботом, сражайтесь с другими игроками и улучшайте свои характеристики!")
 
 # Хэндлер для команды /about
 @dp.message_handler(commands=["about"])
 async def about_handler(message: types.Message):
-    about_text = "Этот бот предназначен для PvP-сражений в стиле PUBG Mobile. Присоединяйтесь к битвам!"
-    await message.reply(about_text)
-
-# Хэндлер для команды /battle
-@dp.message_handler(commands=["battle"])
-async def battle_handler(message: types.Message):
-    await message.reply("PvP-сражение пока в разработке!")
-
-# Хэндлер для команды /accept
-@dp.message_handler(commands=["accept"])
-async def accept_handler(message: types.Message):
-    await message.reply("Вы приняли вызов на PvP-сражение!")
-
-# Хэндлер для команды /attack
-@dp.message_handler(commands=["attack"])
-async def attack_handler(message: types.Message):
-    await message.reply("Вы атаковали соперника!")
-
-# Хэндлер для команды /defend
-@dp.message_handler(commands=["defend"])
-async def defend_handler(message: types.Message):
-    await message.reply("Вы защитились от атаки!")
-
-# Хэндлер для команды /exit
-@dp.message_handler(commands=["exit"])
-async def exit_handler(message: types.Message):
-    await message.reply("Вы вышли из игры.")
-
-# Хэндлер для команды /profile
-@dp.message_handler(commands=["profile"])
-async def profile_handler(message: types.Message):
-    await message.reply("Ваш профиль: Уровень 1, Здоровье 100, Баланс 50 монет.")
-
-# Хэндлер для команды /stats
-@dp.message_handler(commands=["stats"])
-async def stats_handler(message: types.Message):
-    await message.reply("Статистика игрока: Побед 10, Поражений 2.")
-
-# Хэндлер для команды /leaderboard
-@dp.message_handler(commands=["leaderboard"])
-async def leaderboard_handler(message: types.Message):
-    await message.reply("Таблица лидеров: 1. Игрок1 100 очков, 2. Игрок2 90 очков.")
-
-# Хэндлер для команды /health
-@dp.message_handler(commands=["health"])
-async def health_handler(message: types.Message):
-    await message.reply("Ваше текущее здоровье: 100.")
-
-# Хэндлер для команды /shop
-@dp.message_handler(commands=["shop"])
-async def shop_handler(message: types.Message):
-    await message.reply("Магазин открыт: 1. Лечебный набор - 20 монет, 2. Оружие - 50 монет.")
-
-# Хэндлер для команды /buy
-@dp.message_handler(commands=["buy"])
-async def buy_handler(message: types.Message):
-    await message.reply("Вы купили предмет!")
-
-# Хэндлер для команды /sell
-@dp.message_handler(commands=["sell"])
-async def sell_handler(message: types.Message):
-    await message.reply("Вы продали предмет!")
-
-# Хэндлер для команды /inventory
-@dp.message_handler(commands=["inventory"])
-async def inventory_handler(message: types.Message):
-    await message.reply("Ваш инвентарь: Лечебный набор, Пистолет.")
-
-# Хэндлер для команды /equip
-@dp.message_handler(commands=["equip"])
-async def equip_handler(message: types.Message):
-    await message.reply("Вы экипировали пистолет.")
-
-# Хэндлер для команды /unequip
-@dp.message_handler(commands=["unequip"])
-async def unequip_handler(message: types.Message):
-    await message.reply("Вы сняли пистолет.")
-
-# Хэндлер для команды /balance
-@dp.message_handler(commands=["balance"])
-async def balance_handler(message: types.Message):
-    await message.reply("Ваш баланс: 50 монет.")
-
-# Хэндлер для команды /daily
-@dp.message_handler(commands=["daily"])
-async def daily_handler(message: types.Message):
-    await message.reply("Вы получили ежедневный бонус: 10 монет.")
-
-# Хэндлер для команды /upgrade
-@dp.message_handler(commands=["upgrade"])
-async def upgrade_handler(message: types.Message):
-    await message.reply("Вы улучшили оружие!")
-
-# Хэндлер для команды /addcoins
-@dp.message_handler(commands=["addcoins"])
-async def addcoins_handler(message: types.Message):
-    # Только для администраторов
-    if message.from_user.id == 123456789:  # Здесь должен быть ID администратора
-        await message.reply("Вы добавили монеты!")
-    else:
-        await message.reply("У вас нет прав для этой команды.")
-
-# Хэндлер для команды /resetstats
-@dp.message_handler(commands=["resetstats"])
-async def resetstats_handler(message: types.Message):
-    # Только для администраторов
-    if message.from_user.id == 123456789:  # Здесь должен быть ID администратора
-        await message.reply("Статистика игрока сброшена!")
-    else:
-        await message.reply("У вас нет прав для этой команды.")
+    await message.reply("Бот для PvP-сражений в стиле PUBG Mobile!")
 
 # Настройка webhook
 async def on_startup(dispatcher):
@@ -192,7 +88,9 @@ async def on_startup(dispatcher):
 async def on_shutdown(dispatcher):
     logger.info("Удаление вебхука...")
     await bot.delete_webhook()
-    await bot.session.close()
+    # Получаем сессию и закрываем её корректно
+    session = await bot.get_session()
+    await session.close()
 
 if __name__ == "__main__":
     start_webhook(
@@ -203,6 +101,7 @@ if __name__ == "__main__":
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
     )
+
 
 
 
